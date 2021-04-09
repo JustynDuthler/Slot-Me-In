7a4a69d5839b742fea88cdc9d12d6951eda02172
lucaslb1 <lucasbattelle@gmail.com>
2021-04-09 16:36:38 -0700

    Added routing to different pages in front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import Home from './Home';
 
 /**
  *
@@ -6,9 +15,34 @@ import React from 'react';
  */
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Create Account</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/login">
+            <Login/>
+          </Route>
+          <Route path="/register">
+            <Register/>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
