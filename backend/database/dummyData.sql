ce055c1c8d05a7de3faa81e577cdde647019b4ad
PyralIron <williamleber@gmail.com>
2021-04-13 18:37:33 -0700

    fixed dummy user entry

--- backend/database/dummyData.sql
+++ backend/database/dummyData.sql
@@ -1,6 +1,7 @@
 -- Inserts dummy data into Users table for testing
+-- Unhashed password is PaSWord
 INSERT INTO Users(userName, Password, userEmail)
-VALUES ('Jeff', 'PaSWord', 'jeff@ucsc.edu');
+VALUES ('Jeff', '$2b$10$yM6wHn3IDtPttfUKnZ3mge/wTJKcxBnFSPNqm/2DIusW5KPYWNihm', 'jeff@ucsc.edu');
 
 INSERT INTO Businesses(businessID, businessName, Password, phoneNumber, businessEmail)
 VALUES ('00000000-0000-0000-0000-000000000000', 'Test Inc.', 'Pwd', '000-000-0000', 'contact@testinc.com');
