f1e9d318ec37e3dfc9867f69fae3a539d8d45689
PyralIron <williamleber@gmail.com>
2021-04-16 13:33:37 -0700

    login page now uses material ui

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,4 +1,27 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import InputLabel from '@material-ui/core/InputLabel';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import FilledInput from '@material-ui/core/FilledInput';
+import IconButton from '@material-ui/core/IconButton';
+import Container from '@material-ui/core/Container';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/material.css'
 const Auth = require('./libs/Auth');
 
 /**
@@ -7,7 +30,9 @@ const Auth = require('./libs/Auth');
 export default function Login() {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [phoneNumber, setPhoneNumber] = React.useState("");
   const [showBusiness, setForm] = React.useState(false);
+  const [showPassword, setVisibility] = React.useState(false);
   const [flag, setFlag] = React.useState(false);
   /**
    * Handles form submission
@@ -41,38 +66,110 @@ export default function Login() {
           console.log(error);
         });
   };
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.primary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+  const classes = useStyles();
   return (
     <div>
-    {showBusiness && <form onSubmit={handleSubmit}>
-      <label>
-        Business Email:
-        <input type="text" value={email}
-          onChange={(event) => {setEmail(event.target.value);}} />
-      </label><br/>
-      <label>
-        Password:
-        <input type="text" value={password}
-          onChange={(event) => {setPassword(event.target.value);}} />
-      </label><br/>
-      <input type="submit" value="Login" />
-    </form>}
-    {showBusiness && <button onClick={(event) => {setForm(!showBusiness);}}>Toggle User Login</button>}
-    {!showBusiness && <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input type="text" value={email}
-          onChange={(event) => {setEmail(event.target.value);}} />
-      </label><br/>
-      <label>
-        Password:
-        <input type="text" value={password}
-          onChange={(event) => {setPassword(event.target.value);}} />
-      </label><br/>
-      <input type="submit" value="Login" />
-    </form>}
-    {!showBusiness && <button onClick={(event) => {setForm(!showBusiness);}}>Toggle Business Login</button>}
-    {flag && <p>The email or password entered is incorrect.</p>}
-    </div>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} onSubmit={handleSubmit} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              onChange={(event) => {setEmail(event.target.value);}}
+              autoFocus
+            />
 
+            <FormControl className={classes.form} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+              <OutlinedInput
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type={showPassword ? "text" : "password"}
+                id="password"
+                onChange={(event) => {setPassword(event.target.value);}}
+                autoComplete="current-password"
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={(event) => {setVisibility(!showPassword);}}
+                      edge="end"
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" onChange={(event) => {setForm(event.target.checked);}}/>}
+              label="Business Account"
+            />
+            {showBusiness && <PhoneInput
+              inputStyle={{width:'100%'}}
+              country={'us'}
+              value={phoneNumber}
+              onChange={(phone) => {setPhoneNumber(phoneNumber);}}
+            />}
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Container>
+    </div>
   );
 }
