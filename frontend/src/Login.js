8193f393c429043a1b16b9e9f9bf433256f1dca4
Ethan Perlada <eperlada@ucsc.edu>
2021-04-16 14:13:39 -0700

    link to registration page from login page

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -162,7 +162,7 @@ export default function Login() {
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/Register" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
