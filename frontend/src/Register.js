d0797cbbfae689dc0a5a41762fc560b7dd51f3e5
PyralIron <williamleber@gmail.com>
2021-04-16 14:35:55 -0700

    register page tweaks

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -167,20 +167,8 @@ const Auth = require('./libs/Auth');
                color="primary"
                className={classes.submit}
              >
-               Sign In
+               Register
              </Button>
-             <Grid container>
-               <Grid item xs>
-                 <Link href="#" variant="body2">
-                   Forgot password?
-                 </Link>
-               </Grid>
-               <Grid item>
-                 <Link href="#" variant="body2">
-                   {"Don't have an account? Sign Up"}
-                 </Link>
-               </Grid>
-             </Grid>
            </form>
          </div>
        </Container>
