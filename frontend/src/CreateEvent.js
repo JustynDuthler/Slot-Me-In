b0ec63aca43dd1348205aaee21546fa36f68cf32
PyralIron <williamleber@gmail.com>
2021-04-19 12:01:04 -0700

    fancy create event ui

--- frontend/src/CreateEvent.js
+++ frontend/src/CreateEvent.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import EventIcon from '@material-ui/icons/Event';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import Container from '@material-ui/core/Container';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import Context from './Context';
+import DateFnsUtils from '@date-io/date-fns';
+import { DateTimePicker, KeyboardDateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
 const Auth = require('./libs/Auth');
 /**
  * CreateEvent Function
  */
 export default function CreateEvent() {
   const[eventName, changeName] = React.useState("");
-  const[startDate, changeStartDate] = React.useState("");
-  const[startTime, changeStartTime] = React.useState("");
-  const[endDate, changeEndDate] = React.useState("");
-  const[endTime, changeEndTime] = React.useState("");
+  const[startDateTime, changeStartDateTime] = React.useState(null);
+  const[endDateTime, changeEndDateTime] = React.useState(null);
   const[capacity, changeCapacity] = React.useState("");
   const[repeat, changeRepeat] = React.useState(false);
 
@@ -24,8 +43,8 @@ export default function CreateEvent() {
         method: "POST",
         body: JSON.stringify({
           "eventname":eventName,
-          "starttime":new Date(startDate+" "+startTime).toISOString(),
-          "endtime":new Date(endDate+" "+endTime).toISOString(),
+          "starttime":startDateTime.toISOString(),
+          "endtime":endDateTime.toISOString(),
           "capacity":parseInt(capacity),
           "repeat":repeat,
         }),
@@ -39,44 +58,104 @@ export default function CreateEvent() {
       });
     }
   };
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.primary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    dateselect: {
+      width: '100%',
+      marginTop: theme.spacing(2),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+  const classes = useStyles();
+  const validateInput = (event) => {
+    // regex to check for valid email format
+    //const phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+    handleSubmit(event);
+  }
+  if (Auth.JWTHeader() === null) {
+    return <Redirect to={{pathname: '/'}}/>
+  }
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-      <label>
-        Event Name:
-        <input type="text" value={eventName}
-          onChange={(event) => {changeName(event.target.value);}} />
-      </label><br/>
-      <label>
-        Start Date:
-        <input type="text" value={startDate}
-          onChange={(event) => {changeStartDate(event.target.value);}} />
-      </label><br/>
-      <label>
-        Start Time:
-        <input type="text" value={startTime}
-          onChange={(event) => {changeStartTime(event.target.value);}} />
-      </label><br/>
-      <label>
-        End Date:
-        <input type="text" value={endDate}
-          onChange={(event) => {changeEndDate(event.target.value);}} />
-      </label><br/>
-      <label>
-        End Time:
-        <input type="text" value={endTime}
-          onChange={(event) => {changeEndTime(event.target.value);}} />
-      </label><br/>
-      <label>
-        Capacity:
-        <input type="text" value={capacity}
-          onChange={(event) => {changeCapacity(event.target.value);}} />
-      </label><br/>
-      {repeat && <button onClick={(event) => {changeRepeat(!repeat);}}>Repeat</button>}
-      {!repeat && <button onClick={(event) => {changeRepeat(!repeat);}}>Don't Repeat</button>}
-      <input type="submit" value="Create Event" />
-    </form>
-    </div>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <EventIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Create an Event
+          </Typography>
+          <div className={classes.form}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="eventname"
+              label="Event Name"
+              name="eventname"
+              onChange={(event) => {changeName(event.target.value);}}
+              autoFocus
+            />
+            <MuiPickersUtilsProvider utils={DateFnsUtils} className={classes.form}>
+              <DateTimePicker
+                clearable
+                className={classes.dateselect}
+                label="Start Date/Time"
+                inputVariant="outlined"
+                value={startDateTime}
+                onChange={changeStartDateTime}
+              />
+            </MuiPickersUtilsProvider>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <DateTimePicker
+                clearable
+                className={classes.dateselect}
+                label="End Date/Time"
+                inputVariant="outlined"
+                value={endDateTime}
+                onChange={changeEndDateTime}
+              />
+            </MuiPickersUtilsProvider>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="capacity"
+              label="Capacity"
+              name="capacity"
+              onChange={(event) => {changeCapacity(event.target.value);}}
+            />
 
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={validateInput}
+            >
+              Create Event
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </div>
   );
 }
