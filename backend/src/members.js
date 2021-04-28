5bc87977c4204ccae55b63e6444bba2024911694
Justyn Duthler <justynduthler@gmail.com>
2021-04-27 20:23:31 -0700

    adding members now handles cases where user is already a member, given email is not a user of the site, and multiple users

--- backend/src/members.js
+++ backend/src/members.js
@@ -2,11 +2,12 @@ const db = require('./db');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 const auth = require('./auth');
+const { use } = require('./app');
 dotenv.config();
 
 exports.addMembers = async (req, res) => {
     const businessid = req.payload.id;
-    // create an array of userIDs
+    /* create an array of userIDs */
     let userID = []
     console.log(req.body);
     let length = req.body.length;
@@ -15,22 +16,44 @@ exports.addMembers = async (req, res) => {
     for(i = 0; i < length; i++) {
         userID[i] = await db.getUserIDByEmail(req.body[i]);
     }
-    // construct tuples to insert into members table
+    /* get list of already added users */
+    let existingMembers = await db.getMembersForBusiness(businessid);
+    /* construct tuples to insert into members table */
     let memberListString = '';
+    let existFlag = 0;
+    let firstInsert = 0;
     for(i = 0; i < length; i++) {
-        if (i < length -1) {
-            memberListString = memberListString + '(\'' + businessid + '\', \'' + req.body[i] + '\', \'' + userID[i] +'\'), ' ;
-        } else {
-            memberListString = memberListString + '(\'' + businessid + '\', \'' + req.body[i] + '\', \'' + userID[i] +'\')' ;
+        /* set flag if userid is already a member */
+        for (j = 0; j < existingMembers.length; j++) {
+            if(userID[i] == existingMembers[j].userid) {
+                existFlag = 1;
+            }
+        } 
+        /* if userid is already a member or not a user skip it */
+        if (existFlag == 1 || userID[i] == null) {
+            existFlag = 0;
+            continue;
         }
-        
+        if (firstInsert == 0) {
+            memberListString = memberListString + '(\'' + businessid + '\', \'' + req.body[i] + '\', \'' + userID[i] +'\')';
+            firstInsert = 1;
+        } else if (i < length -1) {
+            memberListString = memberListString + ', (\'' + businessid + '\', \'' + req.body[i] + '\', \'' + userID[i] +'\')' ;
+        } else {
+            memberListString = memberListString + ', (\'' + businessid + '\', \'' + req.body[i] + '\', \'' + userID[i] +'\')' ;
+        }
     }
-
-    const insertNum = db.insertMembers(memberListString);
-    if (insertNum == 0) {
-        res.status(400).send();
+    /* if new members insert, else throw 400 */
+    if (memberListString.length != 0) {
+        const insertNum = await db.insertMembers(memberListString);
+        console.log(insertNum);
+        if (insertNum == 0) {
+            res.status(400).send();
+        } else {
+            res.status(200).send();
+        }
     } else {
-        res.status(200).send();
+        res.status(400).send();
     }
 }
 
