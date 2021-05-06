f6d3965995c6a607df5b9af267c0222dd9a1cd14
Justyn Duthler <justynduthler@gmail.com>
2021-05-05 23:30:39 -0700

    insert members now returns memberlist array and delete member returns members object

--- backend/src/routes/members.js
+++ backend/src/routes/members.js
@@ -9,10 +9,17 @@ exports.deleteMember = async (req, res) => {
   const businessid = req.payload.id;
   const email = req.body.email;
   console.log(req.body.email);
+  /* get removed member info */
+  const removedMember = await memberDb.getMemberUserNameID(email);
+  let member = {
+    username: (removedMember === null ? null : removedMember.username),
+    email: email,
+    userid: (removedMember === null ? null : removedMember.userid)
+  }
 
   memberDb.removeMember(businessid, email)
     .then(length => {
-      res.status(200).send();
+      res.status(200).json(member);
     })
     .catch(err => {
       // Couldn't remove the email
@@ -24,10 +31,21 @@ exports.deleteMember = async (req, res) => {
 // Add members to members table
 exports.addMembers = async (req, res) => {
   const businessid = req.payload.id;
-
+  /* fill member list object with email, username, userid */
+  let memberList = [];
+  for (i = 0; i < req.body.length; i++) {
+    const user = await memberDb.getMemberUserNameID(req.body[i]);
+    let member = {
+      username: (user === null ? null : user.username),
+      email: req.body[i],
+      userid: (user === null ? null : user.userid)
+    }
+    memberList.push(member);
+  }
+  console.log(memberList);
   memberDb.insertMembers(req.body, businessid)
   .then((response) => {
-    res.status(200).send();
+    res.status(200).json(memberList);
   })
   .catch((error) => {
     console.log("error in addMembers: " + error.stack);
@@ -43,9 +61,9 @@ exports.getMembers = async (req, res) => {
   for(i = 0; i < emailList.length; i++) {
     const user = await memberDb.getMemberUserNameID(emailList[i]);
     let member = {
-      username: user.username,
+      username: (user === null ? null : user.username),
       email: emailList[i],
-      userid: user.userid
+      userid: (user === null ? null : user.userid)
     }
     memberList.push(member);
   }
