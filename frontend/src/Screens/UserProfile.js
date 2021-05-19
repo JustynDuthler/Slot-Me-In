89dd5c58214f8d82bdf0fb16ca322463a1d6f6b1
Ethan Perlada <eperlada@ucsc.edu>
2021-05-18 22:13:08 -0700

    clicking on event in userprofile redirects to event page instead of embedding in user profile

--- frontend/src/Screens/UserProfile.js
+++ frontend/src/Screens/UserProfile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 import Container from '@material-ui/core/Container';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -11,7 +12,6 @@ import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
 import Context from '../Context';
 import * as Auth from '../libs/Auth';
-import IndividualEvent from '../IndividualEvent';
 import Paper from '@material-ui/core/Paper';
 import DateFnsUtils from '@date-io/date-fns';
 import {DatePicker, MuiPickersUtilsProvider} from '@material-ui/pickers';
@@ -37,8 +37,7 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 // up grouped by business name. The eventList state holds the
 // currently signed up for events and must be updated
 // (by replacing the reference since it is an object) when a
-// user withdraws from an event. eventState is either null or an eventid.
-// If it's an eventid, then display the individualevent page for the event
+// user withdraws from an event.
 /**
  * UserProfile component
  * @return {object} UserProfile JSX
@@ -48,7 +47,6 @@ export default function UserProfile() {
   const [isLoaded, setIsLoaded] = React.useState(false);
   const [userData, setUserData] = React.useState([]);
   const [eventList, setEventList] = React.useState({});
-  const [eventState, setEventState] = React.useState(null);
   const [selectedDate, setSelectedDate] = React.useState(new Date());
   const [showAll, setShowAll] = React.useState(true);
   const [withdrawEventID, setWithdrawEventID] = React.useState('');
@@ -56,6 +54,7 @@ export default function UserProfile() {
   const [withdrawEventValue, setWithdrawEventValue] = React.useState('');
   const [confirmDialog, setConfirmDialog] = React.useState(false);
   const context = React.useContext(Context);
+  const history = useHistory();
 
   /**
    * removeUserAttending
@@ -155,7 +154,7 @@ export default function UserProfile() {
         );
     await Promise.all([userRes, eventRes]);
     setIsLoaded(true);
-  }, [eventState]);
+  }, []);
 
 
   const useStyles = makeStyles((theme) => ({
@@ -284,25 +283,6 @@ export default function UserProfile() {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
     return <div>Loading...</div>;
-  } else if (eventState !== null) {
-    // if the eventState is set to an eventID
-    // then show an individualEvent page with a back button
-    items.push(
-        <div key='event' className={classes.eventStyle}>
-          <Button
-            type='submit'
-            variant='contained'
-            color='primary'
-            onClick={() => {
-              setIsLoaded(null);
-              setEventState(null);
-            }}
-          >
-          Back
-          </Button>
-          <IndividualEvent eventID={eventState}/>
-        </div>,
-    );
   } else if (Object.keys(eventList) == 0) {
     // If there are no events in the event list
     // then put a link to the event signup page
@@ -349,7 +329,7 @@ export default function UserProfile() {
                     key={eventid}
                     onClick={
                       () => {
-                        setEventState(eventid);
+                        history.push('/event/' + eventid);
                       }
                     }
                   >
@@ -454,7 +434,7 @@ export default function UserProfile() {
             {userData.useremail.toLowerCase()}
           </Typography>
           <Grid container justify='center' direction='row' spacing={8}>
-            {eventState === null &&showAll === false&&
+            {showAll === false&&
             <Grid item xs={6} container justify='flex-start'>
               <MuiPickersUtilsProvider utils={DateFnsUtils}>
                 <DatePicker
