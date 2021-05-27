3be160a9b4bf6fe03c27794c8a021cc23a780913
Ethan Perlada <eperlada@ucsc.edu>
2021-05-27 14:11:49 -0700

    added some EventCategories db queries

---
+++ backend/src/db/categoriesDb.js
@@ -0,0 +1,23 @@
+const pool = require('./dbConnection');
+
+exports.getCategories = async () => {
+  const select = 'SELECT * FROM EventCategories';
+  const query = {
+    text: select,
+    values: [],
+  };
+
+  const {rows} = await pool.query(query);
+  return rows;
+};
+
+exports.checkCategoryExists = async (category) => {
+  const select = 'SELECT * FROM EventCategories WHERE category = $1';
+  const query = {
+    text: select,
+    values: [category],
+  };
+
+  const {rows} = await pool.query(query);
+  return (rows.length > 0 ? rows[0] : undefined);
+};
